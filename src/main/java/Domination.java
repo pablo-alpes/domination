c57872fde6ee9ff81fe0ca5e12a5d7fb924e338f
pablo-alpes <pablo.miranda@sfr.fr>
2024-06-26 22:02:26 +0200

    planning: Controller and sequence for the game and development of user stories.

--- src/main/java/Domination.java
+++ src/main/java/Domination.java
@@ -1,5 +1,29 @@
+import model.BoardImpl;
+import service.MapFetchService;
+
+import java.io.FileNotFoundException;
+
 public class Domination {
-    public static void main(String... args) {
-        System.out.println("Domination game");
+    public static void main(String... args) throws FileNotFoundException {
+
+        System.out.println("Domination game -- To conquer the world");
+
+        //Map selection
+        String filename = "spain/spain.map";
+        BoardImpl board = new BoardImpl();
+        board = MapFetchService.translatorToBoard(filename);
+        System.out.println("Let's begin - Default map is <Spain>");
+
+        //ACT
+        /* Game sequence
+        1. Choose the player name: PlayerService
+        2. Assign randomly the countries: OwnershipService
+        3. Show ownerships to each player: GameStatusService
+        4. Assign turns until a player is "alive": TurnsService
+        5. Ask for a move: MoveExecutionService
+        6. Execute the move checking whether is valid: MoveCheckService
+        7. Repeat the cycle until world is conquered by one player:
+         */
+
     }
 }
